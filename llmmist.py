013868b48f65299a45c2e3e241642acc00ec0775
Arnab Chatterjee <19560640+perrin-ay@users.noreply.github.com>
2024-09-10 12:14:28 -0400

    Delete llmmist.py

--- llmmist.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_mistralai import ChatMistralAI
-os.environ['MISTRAL_API_KEY'] = os.getenv('mist')
-
-# per mistral docs models that support tool calling are
-#Mistral Small, Mistral Large, Mixtral 8x22B
-
-llmMist = ChatMistralAI(model="mistral-large-latest", temperature =0)
